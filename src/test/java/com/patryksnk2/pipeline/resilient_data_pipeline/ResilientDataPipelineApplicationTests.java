0208b4459e96680cd51bc2c62422a862247dd26d
Patryk Czerski <pczerski@opi.org.pl>
2025-12-22 14:11:01 +0100

    java setup

---
+++ src/test/java/com/patryksnk2/pipeline/resilient_data_pipeline/ResilientDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryksnk2.pipeline.resilient_data_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientDataPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
