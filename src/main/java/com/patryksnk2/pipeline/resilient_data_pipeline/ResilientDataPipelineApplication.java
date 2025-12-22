52b6098d9161702d0ce5436f493a9c03b3a9b606
Patryk Czerski <pczerski@opi.org.pl>
2025-12-22 13:50:14 +0100

    Initial Spring Boot project structure

---
+++ src/main/java/com/patryksnk2/pipeline/resilient_data_pipeline/ResilientDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.patryksnk2.pipeline.resilient_data_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientDataPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilientDataPipelineApplication.class, args);
+	}
+
+}
