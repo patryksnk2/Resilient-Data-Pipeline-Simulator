156a8bef93450b433de60838f12f423caba2c1e6
patryksnk2 <patrykczerski83@gmail.com>
2026-01-06 18:56:40 +0100

    refactor(exception): preserve root cause in PayloadSerializeException

--- src/main/java/com/patryksnk2/pipeline/resilientdatapipeline/exception/PayloadSerializeException.java
+++ src/main/java/com/patryksnk2/pipeline/resilientdatapipeline/exception/PayloadSerializeException.java
@@ -1,7 +1,7 @@
 package com.patryksnk2.pipeline.resilientdatapipeline.exception;
 
 public class PayloadSerializeException extends RuntimeException {
-    public PayloadSerializeException(String message) {
-        super(message);
+    public PayloadSerializeException(String message,Throwable cause) {
+        super(message,cause);
     }
 }
