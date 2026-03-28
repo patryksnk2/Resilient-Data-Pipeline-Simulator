d4bbe0aa45aa9ff62a853376e8d6d66b0fe4bc69
patryksnk <166032248+patryksnk2@users.noreply.github.com>
2026-03-28 19:29:43 +0100

    test: add integration tests for PipelineServiceImpl (#54)

--- src/main/java/com/patryksnk2/pipeline/resilientdatapipeline/repository/ProcessingResultRepository.java
+++ src/main/java/com/patryksnk2/pipeline/resilientdatapipeline/repository/ProcessingResultRepository.java
@@ -2,6 +2,12 @@ package com.patryksnk2.pipeline.resilientdatapipeline.repository;
 
 import com.patryksnk2.pipeline.resilientdatapipeline.domain.ProcessingResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 public interface ProcessingResultRepository extends JpaRepository<ProcessingResult,Long> {
+    @Query("select p from ProcessingResult p where p.pipelineJob.id = ?1")
+    List<ProcessingResult> findAllByPipelineId(@NonNull Long id);
 }
